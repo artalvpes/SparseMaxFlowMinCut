fa7d8e595f06b141c592da4dade316072ca300c3
Artur Alves Pessoa <artur.a.pessoa@gmail.com>
2019-11-07 16:20:37 -0300

    Replace tabs by spaces to fix identation

--- src/SparseMaxFlowMinCut.jl
+++ src/SparseMaxFlowMinCut.jl
@@ -18,12 +18,12 @@ struct ArcFlow
 end
 
 mutable struct Graph
-	_n::Int
-	adj::Vector{Vector{Int}}
-	val::Vector{Vector{Int}}
+    _n::Int
+    adj::Vector{Vector{Int}}
+    val::Vector{Vector{Int}}
 
-	# position of the opposite arc in the adjacency list of its head node
-	opp::Vector{Vector{Int}}
+    # position of the opposite arc in the adjacency list of its head node
+    opp::Vector{Vector{Int}}
 end
 
 # constructor
@@ -32,17 +32,17 @@ function Graph(n::Int, A::Vector{ArcFlow})
         [Vector{Int}() for i=1:n]
     )
 
-	# add the arcs to the graph
-	for a in A
-		push!(g.adj[a.i], a.j)
-		push!(g.val[a.i], a.f)
-		push!(g.opp[a.i], length(g.opp[a.j])+1)
-		push!(g.adj[a.j], a.i)
-		push!(g.val[a.j], 0)
-		push!(g.opp[a.j], length(g.opp[a.i]))
+    # add the arcs to the graph
+    for a in A
+        push!(g.adj[a.i], a.j)
+        push!(g.val[a.i], a.f)
+        push!(g.opp[a.i], length(g.opp[a.j])+1)
+        push!(g.adj[a.j], a.i)
+        push!(g.val[a.j], 0)
+        push!(g.opp[a.j], length(g.opp[a.i]))
     end
-	
-	return g
+    
+    return g
 end
 
 # find a path p from s to t using only non-zero arcs and return its minimum arc value, and p
@@ -51,53 +51,53 @@ end
 # * if no such a path exist, store in p a list of vertices that are reachable from s and
 #   return 0, and p.
 function find_path(g::Graph, s::Int, t::Int)::Tuple{Int, Vector{Int}}
-	# do a bfs in the graph
-	prev = fill(-2, g._n)
-	q = CircularDeque{Int}(g._n)
-	push!(q, s)
-	prev[s] = -1
+    # do a bfs in the graph
+    prev = fill(-2, g._n)
+    q = CircularDeque{Int}(g._n)
+    push!(q, s)
+    prev[s] = -1
     while !isempty(q)
-		i = popfirst!(q)
+        i = popfirst!(q)
 
-		# check if the sink was reached
-		if i == t
-			# fill the path and return the minimum value
-			minVal = typemax(Int)
-			p = Vector{Int}()
-			while prev[i] != -1
-				push!(p, prev[i])
-				k = g.opp[i][prev[i]]
-				i = g.adj[i][prev[i]]
-				if (minVal > g.val[i][k])
-					minVal = g.val[i][k]
-				end
-			end
-			return minVal, p
-		end
+        # check if the sink was reached
+        if i == t
+            # fill the path and return the minimum value
+            minVal = typemax(Int)
+            p = Vector{Int}()
+            while prev[i] != -1
+                push!(p, prev[i])
+                k = g.opp[i][prev[i]]
+                i = g.adj[i][prev[i]]
+                if (minVal > g.val[i][k])
+                    minVal = g.val[i][k]
+                end
+            end
+            return minVal, p
+        end
 
-		# check the adjacent vertices
-		l = length(g.adj[i])
-		for k in 1:l
-			if g.val[i][k] == 0
-				continue
-			end
-			j = g.adj[i][k]
-			if prev[j] != -2
-				continue
-			end
-			prev[j] = g.opp[i][k]
-			push!(q, j)
-		end
-	end
+        # check the adjacent vertices
+        l = length(g.adj[i])
+        for k in 1:l
+            if g.val[i][k] == 0
+                continue
+            end
+            j = g.adj[i][k]
+            if prev[j] != -2
+                continue
+            end
+            prev[j] = g.opp[i][k]
+            push!(q, j)
+        end
+    end
 
-	# store in p a list of vertices that are reachable from s and return 0
-	p = Vector{Int}()
-	for i = 1:g._n
-		if prev[i] != -2
-			push!(p, i)
-		end
-	end
-	return 0, p
+    # store in p a list of vertices that are reachable from s and return 0
+    p = Vector{Int}()
+    for i = 1:g._n
+        if prev[i] != -2
+            push!(p, i)
+        end
+    end
+    return 0, p
 end
 
 # find a minimum s-t cut in the graph instance and return the cut value
@@ -107,28 +107,28 @@ function find_maxflow_mincut(instance::Graph, s::Int, t::Int
         return typemax(Int), ArcFlow[], Bool[]
     end
 
-	# make a copy of the instance graph as the residual graph
-	residual = deepcopy(instance)
+    # make a copy of the instance graph as the residual graph
+    residual = deepcopy(instance)
 
-	# iterate finding paths from s to t
-	p = Vector{Int}()
-	flow, p = find_path(residual, s, t)
-	maxFlow = 0
-	while (flow > 0)
-		# pass flow through the path
-		i = t
-		ll = length(p)
-		for l in 1:ll
-			ki = p[l]
-			j = residual.adj[i][ki]
-			kj = residual.opp[i][ki]
-			residual.val[j][kj] -= flow
-			residual.val[i][ki] += flow
-			i = j
-		end
-		@assert i == s
-		maxFlow += flow
-		flow, p = find_path(residual, s, t)
+    # iterate finding paths from s to t
+    p = Vector{Int}()
+    flow, p = find_path(residual, s, t)
+    maxFlow = 0
+    while (flow > 0)
+        # pass flow through the path
+        i = t
+        ll = length(p)
+        for l in 1:ll
+            ki = p[l]
+            j = residual.adj[i][ki]
+            kj = residual.opp[i][ki]
+            residual.val[j][kj] -= flow
+            residual.val[i][ki] += flow
+            i = j
+        end
+        @assert i == s
+        maxFlow += flow
+        flow, p = find_path(residual, s, t)
     end
     
     # build the vector of arc flows
@@ -144,13 +144,13 @@ function find_maxflow_mincut(instance::Graph, s::Int, t::Int
         end
     end
 
-	# here, p contains a list of vertices in the cut... convert it the boolean vector cut
-	cut = zeros(Bool, residual._n)
-	ll = length(p)
-	for l in 1:ll
-		cut[p[l]] = true
-	end
-	return maxFlow, flows, cut
+    # here, p contains a list of vertices in the cut... convert it the boolean vector cut
+    cut = zeros(Bool, residual._n)
+    ll = length(p)
+    for l in 1:ll
+        cut[p[l]] = true
+    end
+    return maxFlow, flows, cut
 end
 
 end # module
